3328667c9ad61f6c817501cc906afdd934bd600c
saketverma-03 <saketverma831@gmail.com>
2023-08-11 21:28:05 +0530

    added: theme provier for dark ligh ttheme mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./Router.jsx";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <ThemeProvider>
+      <RouterProvider router={routes} />
+    </ThemeProvider>
   </React.StrictMode>
 );
+
+function ThemeProvider({ children }) {
+  const theme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  console.log(theme);
+  return (
+    <div className="body" data-theme={"light"}>
+      {children}
+    </div>
+  );
+}
