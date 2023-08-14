9df13cdadd2a43e0babaced2ddb8e07ded8969f8
saketverma-03 <saketverma831@gmail.com>
2023-08-14 11:43:25 +0530

    Added: apis for getAllprojects and getOneProjects code

--- src/server/projects.js
+++ src/server/projects.js
@@ -25,7 +25,7 @@ export async function createProject(id, project) {
 export async function getAllProjects(id) {
   const params = {
     method: "get",
-    url: `${basturl}/getAll/${id}`,
+    url: `${basturl}/projects/getAll`,
     headers: {
       "Content-Type": "application/json",
     },
@@ -38,10 +38,23 @@ export async function getAllProjects(id) {
 export async function getOneProject(id) {
   const params = {
     method: "get",
-    url: `${basturl}/getOne/${id}`,
+    url: `${basturl}/projects/getOne/${id}`,
     headers: {
       "Content-Type": "application/json",
     },
+  };
+  const res = await axios(params);
+  return { data: res.data, message: res.message };
+}
+
+export async function updateOneProjectCode(data, id) {
+  const params = {
+    method: "put",
+    url: `${basturl}/projects/updateOne/code/${id}`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data,
   };
   const res = await axios(params);
   return { data: res.data, message: res.message };
